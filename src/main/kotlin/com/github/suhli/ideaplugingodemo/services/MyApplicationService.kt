d77d43dc8104fbf5ddc3496ec224af08f33a79d9
GitHub Action <action@github.com>
2022-09-28 01:44:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/suhli/ideaplugingodemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/suhli/ideaplugingodemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.suhli.ideaplugingodemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.suhli.ideaplugingodemo.MyBundle
 
 class MyApplicationService {
 
