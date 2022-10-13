f450379edc7a9f6541bb760c4567e7e8a05b1ef8
reko <lishuhang@dsj.wecom.work>
2022-10-13 15:17:56 +0800

    feat: protofile add import go module support

--- src/main/kotlin/com/github/suhli/ideagokratosplugin/helper/PbHelper.kt
+++ src/main/kotlin/com/github/suhli/ideagokratosplugin/helper/PbHelper.kt
@@ -3,38 +3,87 @@ package com.github.suhli.ideagokratosplugin.helper
 import com.github.suhli.ideagokratosplugin.extends.KratosTask
 import com.github.suhli.ideagokratosplugin.pb.KratosPbAction
 import com.github.suhli.ideagokratosplugin.pb.KratosPbClientAction
+import com.goide.sdk.GoPackageUtil
 import com.goide.sdk.GoSdkUtil
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.util.ExecUtil
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.progress.runBackgroundableTask
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.protobuf.ide.settings.PbProjectSettings
+import com.intellij.protobuf.jvm.PbJavaOuterClassIndex
 import com.intellij.protobuf.lang.PbFileType
 import com.intellij.protobuf.lang.psi.PbFile
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
+import com.intellij.psi.ResolveState
 import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.util.indexing.FileBasedIndex
 import java.nio.charset.Charset
-private var LOG:Logger? = null
+
+private var LOG: Logger? = null
 private fun getLogger(): Logger {
-    if(LOG == null){
+    if (LOG == null) {
         LOG = Logger.getInstance("PbHelper")
     }
     return LOG!!
 }
-private fun findDependency(file: PsiFile): List<String> {
+
+private fun findModules(file: PsiFile): HashSet<String> {
+    val result = hashSetOf<String>()
     if (file !is PbFile) {
-        return arrayListOf<String>()
+        return result
     }
-    val comments = file.children.filter { v -> v is PsiComment && v.text.contains("depends:") }
-    return comments.map { v ->
-        val text = v.text
+    val project = file.project
+    val moduleComments = file.children.filter { v -> v is PsiComment && v.text.contains("modules:") }
+    val settings = PbProjectSettings.getInstance(project)
+    val entries = arrayListOf<PbProjectSettings.ImportPathEntry>()
+    entries.addAll(settings.importPathEntries)
+    val toAddModule = arrayListOf<PbProjectSettings.ImportPathEntry>()
+    for(comment in moduleComments){
+        val text = comment.text
+        val match = Regex("modules:(.+)").find(text)
+        val module = match?.groupValues?.find { m -> !m.contains("modules") } ?: ""
+        val pkg = GoPackageUtil.findByImportPath(module, project, null, ResolveState.initial())
+        if(pkg.isEmpty()) continue
+        val location = pkg.first().directories.first().path
+        val entry = settings.importPathEntries.find { e ->
+            e.location == location
+        }
+        if(entry == null){
+            val e = PbProjectSettings.ImportPathEntry("file://$location","")
+            entries.add(e)
+            toAddModule.add(e)
+        }
+        result.add("--proto_path=$location")
+    }
+    if(toAddModule.isNotEmpty()){
+        settings.importPathEntries = entries
+    }
+    return result
+}
+
+private fun findDependency(file: PsiFile): HashSet<String> {
+    val result = hashSetOf<String>()
+    if (file !is PbFile) {
+        return result
+    }
+    val dependsComments = file.children.filter { v -> v is PsiComment && v.text.contains("depends:") }
+    for(comment in dependsComments){
+        val text = comment.text
         val match = Regex("depends:(.+)").find(text)
         val path = match?.groupValues?.find { m -> !m.contains("depends") } ?: ""
-        if (path.isNotEmpty()) "--proto_path=${path}" else ""
-    }.filter { v -> v.isNotEmpty() }
+        if (path.isNotEmpty()) {
+            result.add("--proto_path=${path}")
+        }
+    }
+    return result
 }
 
 fun genPbTask(file: PsiFile): KratosTask? {
@@ -43,7 +92,10 @@ fun genPbTask(file: PsiFile): KratosTask? {
     }
     val project = file.project
     val parentPath = DirHelper.relativeToRoot(file.parent ?: return null) ?: return null
-    val otherPaths = findDependency(file)
+    val otherPaths = hashSetOf<String>()
+    otherPaths.addAll(findDependency(file))
+    otherPaths.addAll(findModules(file))
+
     val cmds = arrayListOf("protoc")
     cmds.addAll(otherPaths)
     cmds.add("--proto_path=${project.basePath}/$parentPath")
@@ -97,5 +149,6 @@ fun genAllPb(p: Project): List<KratosTask> {
             tasks.add(genPbTask(file) ?: continue)
         }
     }
+    PbProjectSettings.notifyUpdated(p)
     return tasks
 }
--- src/main/kotlin/com/github/suhli/ideagokratosplugin/pb/KratosPbAction.kt
+++ src/main/kotlin/com/github/suhli/ideagokratosplugin/pb/KratosPbAction.kt
@@ -2,10 +2,13 @@ package com.github.suhli.ideagokratosplugin.pb
 
 import com.github.suhli.ideagokratosplugin.helper.genPbTask
 import com.github.suhli.ideagokratosplugin.helper.runKratosTaskInBackground
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.progress.runBackgroundableTask
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.protobuf.ide.settings.PbProjectSettings
 import com.intellij.psi.PsiFile
 
 class KratosPbAction : DumbAwareAction("Run Kratos Config") {
@@ -18,6 +21,7 @@ class KratosPbAction : DumbAwareAction("Run Kratos Config") {
         val file: PsiFile = e.getData(CommonDataKeys.PSI_FILE) ?: return
         val task = genPbTask(file) ?: return
         val project = e.project ?: return
+        PbProjectSettings.notifyUpdated(project)
         runKratosTaskInBackground("generate proto buffer", project, arrayListOf(task))
     }
 }
