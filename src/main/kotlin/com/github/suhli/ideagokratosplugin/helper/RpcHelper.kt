ac7aba224af2cfe19cf8704611539a3faa0716a0
shuhangli <>
2023-01-05 16:54:37 +0800

    feat: add proto quick http request with params

--- src/main/kotlin/com/github/suhli/ideagokratosplugin/helper/RpcHelper.kt
+++ src/main/kotlin/com/github/suhli/ideagokratosplugin/helper/RpcHelper.kt
@@ -6,50 +6,100 @@ import com.intellij.ide.scratch.ScratchFileService
 import com.intellij.ide.scratch.ScratchRootType
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.protobuf.lang.psi.PbServiceDefinition
 import com.intellij.protobuf.lang.psi.PbServiceMethod
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.util.PathUtil
 
-fun createRpcInRestClient(element:PsiElement){
+
+private fun getFieldFromProtobuf(element: PbServiceMethod): List<KratosRpcField> {
+    val arg = element.serviceMethodTypeList.first().messageTypeName
+    return KratosRpcField.getFieldsInMessageTypeName(arg)
+}
+
+
+fun createHttpInRestClient(element: PsiElement, method: String, path: String) {
     if (element is PbServiceMethod) {
-        val arg = element.serviceMethodTypeList.first().messageTypeName
-        val fields = KratosRpcField.getFieldsInMessageTypeName(arg)
+        var p = path
+        val svc = element.parent.parent as PbServiceDefinition
+        val pkg = element.pbFile.packageStatement!!.packageName!!.text
+
+        var fields = getFieldFromProtobuf(element)
+        val regex = Regex("\\{.+\\}")
+        if (path.contains(regex)) {
+            val matches = regex.findAll(path)
+            for (m in matches) {
+                val value = m.value.trim { c->c == '{' || c == '}' }
+                fields = fields.filter { v -> v.name != value }
+            }
+        }
+        var body = ""
+        if (method == "get" || method == "delete") {
+            if (fields.isNotEmpty()) {
+                p += "?" + fields.joinToString("&") { v -> v.name + "=" }
+            }
+        } else {
+            body = "{\n" + fields.joinToString(",\n") { v -> v.toJson() } + "\n}\n"
+        }
+        var req = """
+         ### ${pkg}.${svc.name}.${element.name}
+         ${method.uppercase()} localhost:8000${p}
+         """.trimIndent()
+        if(body.isNotEmpty()){
+            req += "\n\n" + body
+        }
+        val project = element.project
+        writeRequest(project, "\n" + req)
+    }
+}
+
+private fun findOrCreateScratchRequest(project: Project): PsiFile? {
+    val fileName = PathUtil.makeFileName("kratos-api", HttpRequestFileType.INSTANCE.defaultExtension)
+    val fileService = ScratchFileService.getInstance()
+    val file: VirtualFile?
+    try {
+        file = fileService.findFile(
+            ScratchRootType.getInstance(),
+            fileName,
+            ScratchFileService.Option.create_if_missing
+        )
+    } catch (e: Exception) {
+        return null
+    }
+    val editorManager = FileEditorManager.getInstance(project)
+    editorManager.openFile(file, true)
+    return PsiManager.getInstance(project).findFile(file)
+}
+
+private fun writeRequest(project: Project, request: String) {
+    val psi = findOrCreateScratchRequest(project) ?: return
+    val documentManager = PsiDocumentManager.getInstance(project)
+    val doc = documentManager.getDocument(psi)!!
+    val applicationManager = ApplicationManager.getApplication()
+    applicationManager.runWriteAction {
+        doc.insertString(doc.textLength, request)
+        documentManager.commitDocument(doc)
+    }
+}
+
+fun createRpcInRestClient(element: PsiElement) {
+    if (element is PbServiceMethod) {
         val project = element.project
         val method = element.nameIdentifier?.text ?: return
         val svc = element.parent.parent as PbServiceDefinition
         val pkg = element.pbFile.packageStatement!!.packageName!!.text
 
+        val fields = getFieldFromProtobuf(element)
         val body = "{\n" + fields.joinToString(",\n") { v -> v.toJson() } + "\n}\n"
-        val fileName = PathUtil.makeFileName("kratos-api", HttpRequestFileType.INSTANCE.defaultExtension)
-        val fileService = ScratchFileService.getInstance()
-        var file: VirtualFile?
-        try {
-            file = fileService.findFile(
-                ScratchRootType.getInstance(),
-                fileName,
-                ScratchFileService.Option.create_if_missing
-            )
-        } catch (e: Exception) {
-            return
-        }
-        val psi = PsiManager.getInstance(project).findFile(file)!!
-        val documentManager = PsiDocumentManager.getInstance(project)
-        val doc = documentManager.getDocument(psi)!!
-        val applicationManager = ApplicationManager.getApplication()
-        val editorManager = FileEditorManager.getInstance(project)
-        editorManager.openFile(file, true)
         val request = "\n" + """
-                    ### ${pkg}.${svc.name}
-                    GRPC localhost:9000/$pkg.${svc.name}/$method
+                ### ${pkg}.${svc.name}.${element.name}
+                GRPC localhost:9000/$pkg.${svc.name}/$method
                 """.trimIndent() + "\n\n" + body
-        applicationManager.runWriteAction {
-            doc.insertString(doc.textLength, request)
-            documentManager.commitDocument(doc)
-        }
-
+        writeRequest(project, request)
     }
 }
--- src/main/kotlin/com/github/suhli/ideagokratosplugin/marker/RunGrpcMarkerProvider.kt
+++ src/main/kotlin/com/github/suhli/ideagokratosplugin/marker/RunGrpcMarkerProvider.kt
@@ -1,34 +1,59 @@
 package com.github.suhli.ideagokratosplugin.marker
 
+import com.github.suhli.ideagokratosplugin.helper.createHttpInRestClient
 import com.github.suhli.ideagokratosplugin.helper.createRpcInRestClient
 import com.intellij.codeInsight.daemon.GutterIconNavigationHandler
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.editor.markup.GutterIconRenderer
-import com.intellij.protobuf.lang.psi.ProtoLeafElement
+import com.intellij.protobuf.lang.annotation.Proto3Annotator
+import com.intellij.protobuf.lang.psi.*
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.childrenOfType
+import com.intellij.psi.util.findParentOfType
 import java.awt.event.MouseEvent
 import java.util.function.Supplier
 
 class RunGrpcMarkerProvider : LineMarkerProvider {
-
+    private val HTTP_KEYS = arrayListOf("get", "put", "post", "delete", "patch")
 
 
     override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
         if (element is ProtoLeafElement && element.text == "rpc") {
-            val handler = object :GutterIconNavigationHandler<ProtoLeafElement>{
-                override fun navigate(e: MouseEvent?, elt: ProtoLeafElement?) {
-                    createRpcInRestClient(element.parent)
-                }
-            }
-            return LineMarkerInfo<ProtoLeafElement>(
+            val handler =
+                GutterIconNavigationHandler<ProtoLeafElement> { _, _ -> createRpcInRestClient(element.parent) }
+            return LineMarkerInfo(
                 element,
                 element.textRange,
                 AllIcons.RunConfigurations.TestState.Run,
-                null, handler, GutterIconRenderer.Alignment.LEFT,
-                Supplier { "RUN GRPC" }
-            )
+                null, handler, GutterIconRenderer.Alignment.LEFT
+            ) { "RUN GRPC" }
+        }
+        if (element is PbOptionName && element.text == "(google.api.http)") {
+            val expr = element.findParentOfType<PbOptionExpression>() ?: return null
+            val agg = expr.childrenOfType<PbAggregateValue>()[0]
+            val texts = agg.childrenOfType<PbTextField>()
+            for (text in texts) {
+                val key = text.children[0].text
+                if (HTTP_KEYS.contains(key)) {
+                    val path = (text.children[1] as PbTextStringValue).value
+                    val parent = text.findParentOfType<PbServiceMethod>() ?: continue
+                    val handler =
+                        GutterIconNavigationHandler<PbOptionName> { _, _ -> createHttpInRestClient(parent,key,path) }
+                    return LineMarkerInfo(
+                        element,
+                        element.textRange,
+                        AllIcons.RunConfigurations.TestState.Run,
+                        null, handler, GutterIconRenderer.Alignment.LEFT
+                    ) { "RUN HTTP" }
+                }
+            }
+            val key = texts[0].text
+            if (HTTP_KEYS.contains(key)) {
+                val path = texts[1].text
+                print(path)
+            }
         }
         return null
     }
